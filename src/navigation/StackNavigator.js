583ec6963b31eed7b094def10ca18883b620bad7
teodelinalm <tlagosmarmol@udesa.edu.ar>
2025-11-03 11:08:42 -0300

    navegacion

---
+++ src/navigation/StackNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from '../screens/Login';
+import Register from '../screens/Register';
+import TabNavigator from './TabNavigator';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+            <Stack.Screen name='Register' component={Register} options={{headerShown: false}}/>
+            <Stack.Screen name='TabNavigator' component={TabNavigator} options={{headerShown: false}}/>
+        </Stack.Navigator>
+    )
+}
---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default class TabNavigator extends Component {
+  render() {
+    return (
+      <Tab.Navigator>
+      </Tab.Navigator>
+    )
+  }
+}
